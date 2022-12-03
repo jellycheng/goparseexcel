9127ca36b64d74d273ef9a7a3e3f0f1e0e78e8dd
程金盛 <jinsheng.cheng@handeson.com>
2022-12-03 20:06:28 +0800

    add func

--- http.go
+++ http.go
@@ -2,6 +2,7 @@ package goparseexcel
 
 import (
 	"github.com/jellycheng/gcurl"
+	"github.com/jellycheng/gosupport"
 	"github.com/jellycheng/gosupport/sign"
 )
 
@@ -20,6 +21,31 @@ func PostJson(reqUrl, strJson, signSecret string) (error, string) {
 		JSON: strJson,
 	}); err != nil {
 		return err, body
+	} else {
+		res, _ := resp.GetBody()
+		return nil, res.GetContents()
+	}
+}
+
+func PostJsonV2(reqUrl, strJson, signSecret string, headers map[string]interface{}, querys map[string]interface{}) (error, string) {
+	signStr := sign.BodyContentMd5Sign(strJson, signSecret)
+	headersMap := gosupport.NewDataManage()
+	headersMap.Set("User-Agent", "gcurl/1.0")
+	headersMap.Set("sign", signStr)
+	for k, v := range headers {
+		headersMap.Set(k, v)
+	}
+	queryMap := gosupport.NewDataManage()
+	queryMap.Set("sign", signStr)
+	for k, v := range querys {
+		queryMap.Set(k, v)
+	}
+	if resp, err := gcurl.Post(reqUrl, gcurl.Options{
+		Headers: headersMap.GetData(),
+		Query:   queryMap.GetData(),
+		JSON:    strJson,
+	}); err != nil {
+		return err, ""
 	} else {
 		res, _ := resp.GetBody()
 		return nil, res.GetContents()
